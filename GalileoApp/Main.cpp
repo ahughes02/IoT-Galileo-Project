0798cfe218a5a8002c04ce662037eddb99474454
Austin Hughes <ahughes02@live.com>
2014-10-01 09:44:31 -0400

    Will respond with temperature when 's' is received from serial

--- GalileoApp/Main.cpp
+++ GalileoApp/Main.cpp
@@ -11,8 +11,14 @@ int _tmain(int argc, _TCHAR* argv[])
     return RunArduinoSketch();
 }
 
+// Classes
 NetworkSerial netSerial;
+
+// Variables
 float temperature = 1.0f;
+byte incByte; 		// stores the next byte in the serial buffer
+char test; 		// test character to be compared in the while loop
+int exitLoop = 0; 	// variable is set to 1 when valid buffer is stored
 
 void setup()
 {
@@ -22,7 +28,24 @@ void setup()
 // the loop routine runs over and over again forever
 void loop()
 {
+	// Wait for message
+	exitLoop = 0;
+	while (exitLoop != 1)
+	{
+		incByte = netSerial.read(); // get the next byte
+
+		test = char(incByte); // store it as a character 
+
+		if (test == 's') // if the next byte is a '#' then its the start of a new string
+		{
+			exitLoop = 1;
+		}
+		else
+		{
+			Sleep(100);
+		}
+	}
+
 	temperature = (float)analogRead(-1);
 	netSerial.print(temperature);
-	Sleep(1000);
 }
